72c6362ce8a8e2b9785c0d4d44d96bfc44901024
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-05-27 22:38:40 +0430

    define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesComponent } from '../Components/articles/articles.component';
+import { HomeComponent } from '../Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'articles', component: ArticlesComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
